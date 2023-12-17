c60028bb233e9b9c973752f562bae5671b522b77
Antonio Marchese <01645194400@estudante.ufjf.br>
2023-12-17 18:25:30 -0300

    feature: adicionando seção de projetos

--- src/components/projects.tsx
+++
@@ -1,9 +0,0 @@
-import SectionLayout from "./sectionLayout";
-
-export default function ProjectsSection() {
-  return (
-    <SectionLayout id="projects" title="projects">
-      <div></div>
-    </SectionLayout>
-  );
-}
